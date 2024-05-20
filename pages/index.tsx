77960ec8974fd63a4fd420764860025ced0214e8
Jean Patimeteeporn <jpatimet@gmail.com>
2024-05-20 15:40:01 -0500

    changed color scheme of landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,20 @@ import { ReactNode } from "react";
 
 export default function Index() {
   return (
-    <div className="w-full pt-60 min-h-screen flex items-center justify-center">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl md:text-6xl lg:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-blue-600 mb-4">
+    <div className="w-full pt-20 md:pt-40 bg-beige-100 min-h-screen flex items-center justify-center">
+      <div className="container mx-auto px-4 flex flex-col items-center">
+        <img
+          className="object-cover mb-8 max-w-full h-auto mx-auto"
+          src="/images/1.png"
+          alt="Cover Image"
+        />
+        <div className="text-center">
+          <h1 className="text-3xl md:text-6xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-r from-orange-400 to-orange-500 hover:animate-bounce mb-4">
             welcome to your travel notebook!
           </h1>
-          <p className="text-xl md:text-3xl lg:text-5xl text-center text-blue-300 mb-12 hover:motion-safe:animate-bounce">
-            drift notes
+          <p className="text-xl md:text-3xl lg:text-5xl text-orange-300 mb-12">
+            plan your days for worry-free trips
           </p>
-          <img
-            className="object-cover mb-8 max-w-full h-auto mx-auto"
-            src="/images/1.png"
-            alt="Cover Image"
-          />
         </div>
       </div>
     </div>
