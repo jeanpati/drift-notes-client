a55d83677673cc29322a2315613d8acd5d95c0d2
Jean Patimeteeporn <jpatimet@gmail.com>
2024-06-10 15:02:04 -0500

    display google maps

--- components/trip/map.tsx
+++ components/trip/map.tsx
@@ -1,12 +1,29 @@
+import { Loader } from "@googlemaps/js-api-loader";
 import React, { useEffect } from "react";
 
 export function Map() {
-  const mapRef = React.useRef(null);
+  const mapRef = React.useRef<HTMLDivElement>(null);
   useEffect(() => {
     const initMap = async () => {
-      console.log("map init");
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: "weekly",
+      });
+      const { Map } = await loader.importLibrary("maps");
+      const position = {
+        lat: 43.642693,
+        lng: -79.3871189,
+      };
+      //map options
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 17,
+        mapId: "MY_NEXTJS_MAPID",
+      };
+      //setup map
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
     };
     initMap();
   }, []);
-  return <h1>Google Maps</h1>;
+  return <div style={{ height: "600px" }} ref={mapRef} />;
 }
--- pages/trips/[id]/index.tsx
+++ pages/trips/[id]/index.tsx
@@ -133,8 +133,8 @@ export default function TripDetails() {
                     .map((day: Day) => (
                       <DayColumn key={day.id} day={day} />
                     ))}
-                  <Map />
                 </div>
+                <Map />
               </div>
             </div>
           </div>
