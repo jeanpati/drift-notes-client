9fd0e43e370337caf6e1c43f9fa01c6935e896d5
Jean Patimeteeporn <jpatimet@gmail.com>
2024-06-17 15:43:29 -0500

    set up map with autocomplete input

--- components/trip/map.tsx
+++ components/trip/map.tsx
@@ -1,41 +1,57 @@
-import { Loader } from "@googlemaps/js-api-loader";
-import React, { useEffect } from "react";
+import { Library, Loader } from "@googlemaps/js-api-loader";
+import React, { useEffect, useRef, useState } from "react";
+import { LatLong } from "../../types";
+import { useJsApiLoader } from "@react-google-maps/api";
 
-export function Map() {
-  const mapRef = React.useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
-        version: "weekly",
-      });
+const libs: Library[] = ["core", "maps", "places", "marker"];
 
-      const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
-        loader.importLibrary("maps"),
-        loader.importLibrary("marker"),
-      ]);
+export function Map(latlong: LatLong) {
+  // const mapRef = React.useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [autoComplete, setAutoComplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+    libraries: libs,
+  });
+  const mapRef = useRef<HTMLDivElement>(null);
+  const placeAutoCompleteRef = useRef<HTMLInputElement>(null);
 
-      const position = {
-        lat: 43.642693,
-        lng: -79.3871189,
-      };
-      //map options
-      const mapOptions: google.maps.MapOptions = {
-        center: position,
-        zoom: 17,
-        mapId: "MY_NEXTJS_MAPID",
-      };
-      //setup map
-      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+  useEffect(() => {}, []);
 
-      // add a marker
-      const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        gmpClickable: true,
-      });
-    };
-    initMap();
-  }, []);
-  return <div style={{ height: "600px" }} ref={mapRef} />;
+  // useEffect(() => {
+  //   const initMap = async () => {
+  //     const loader = new Loader({
+  //       apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+  //       version: "weekly",
+  //     });
+
+  //     const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+  //       loader.importLibrary("maps"),
+  //       loader.importLibrary("marker"),
+  //     ]);
+
+  //     const position = {
+  //       lat: 43.642693,
+  //       lng: -79.3871189,
+  //     };
+  //     //map options
+  //     const mapOptions: google.maps.MapOptions = {
+  //       center: position,
+  //       zoom: 17,
+  //       mapId: "MY_NEXTJS_MAPID",
+  //     };
+  //     //setup map
+  //     const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+
+  //     // add a marker
+  //     const marker = new AdvancedMarkerElement({
+  //       map: map,
+  //       position: position,
+  //       gmpClickable: true,
+  //     });
+  //   };
+  //   initMap();
+  // }, []);
+  // return <div style={{ height: "600px" }} ref={mapRef} />;
 }
