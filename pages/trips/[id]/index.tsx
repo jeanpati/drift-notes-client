91339136ae5e2e6479c5abfde3cc8c34ac0451cc
Jean Patimeteeporn <jpatimet@gmail.com>
2024-06-20 15:57:33 -0500

    added latitude and longitude to dependency array

--- pages/trips/[id]/index.tsx
+++ pages/trips/[id]/index.tsx
@@ -38,7 +38,7 @@ export default function TripDetails() {
     };
 
     fetchCoordinates();
-  }, [trip?.city]);
+  }, [trip?.city, trip?.latitude, trip?.longitude]);
 
   const {
     data: allDays,
